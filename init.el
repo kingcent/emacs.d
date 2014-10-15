2b832740c2a23997dc72b094b401d22e0155713a
kc_woqu_desk <kc_woqu_desk@hp.com>
2014-10-15 20:03:43 +0800

    add gtd

--- init.el
+++ init.el
@@ -137,7 +137,9 @@
 ;; misc has some crucial tools I need immediately
 (require 'init-misc)
 
-;;===kingcent adding begin===
+;;==========kingcent adding begin==========
+
+
 ;-ÉèÖÃÐÐºÅ
 (require 'linum)
 (setq linum-format "%3d ")
@@ -153,47 +155,7 @@
 character 
 (font-spec :family "Microsoft YaHei" :size 13))) 
 
-
-;;---for org gtd start---
-;;refer to this page:http://blog.chinaunix.net/uid-26185912-id-3317793.html
-(require 'remember)
-(require 'org-remember)
-(global-set-key "\C-cc" 'remember)
-(setq GTD_PATH "e:/kuaipan/emacs-files/gtd/")
-
-(org-remember-insinuate)
-(setq org-directory GTD_PATH)
-(setq org-remember-templates '(
-("Task" ?t "** TODO %? %t\n %i\n %a" (concat GTD_PATH "inbox.org") "Tasks")
-("Book" ?c "** %? %t\n %i\n %a" (concat GTD_PATH "inbox.org") "Book")
-("Calendar" ?c "** %? %t\n %i\n %a" (concat GTD_PATH "inbox.org") "Calender")
-("Project" ?p "** %? %t\n %i\n %a" (concat GTD_PATH "inbox.org") "Project")))
-(setq org-default-notes-file (concat org-directory "inbox.org"))
-;;设置TODO关键字
-(setq org-todo-keywords
-(list "TODO(t)" "|" "CANCELED(c)" "DONE(d)"))
-;; 将项目转接在各文件之间，方便清理和回顾。
-(custom-set-variables
-'(org-refile-targets
-(quote
-(("inbox.org" :level . 1)("canceled.org" :level . 1) ("finished.org":level . 1))
-)))
-;; 快速打开inbox
-(defun inbox() (interactive) (find-file (concat GTD_PATH "inbox.org")))
-(global-set-key "\C-cz" 'inbox)
-;; 快速启动 agenda-view
-(define-key global-map "\C-ca" 'org-agenda-list)
-(define-key global-map "\C-ct" 'org-todo-list)
-(define-key global-map "\C-cm" 'org-tags-view)
-;;显示他们的内容
-(setq org-agenda-files
-    (list (concat GTD_PATH "inbox.org")
-          (concat GTD_PATH "canceled.org")
-          (concat GTD_PATH "finished.org")
-    )
-)
-;;---for org gtd end---
-
+(require 'init-gtd11)
 ;;==========kingcent adding end============
 
 (setq idle-require-idle-delay 3)
---
+++ lisp/init-gtd11.el
@@ -0,0 +1,47 @@
+
+;;---for org gtd start---
+;;refer to this page:http://blog.chinaunix.net/uid-26185912-id-3317793.html
+(require 'org-capture)
+(global-set-key "\C-cc" 'org-capture)
+(setq GTD_PATH "e:/kuaipan/emacs-files/gtd11/")
+
+(org-remember-insinuate)
+(setq org-directory GTD_PATH)
+(setq org-capture-templates 
+    '(
+        ("n" "New" entry (file (concat GTD_PATH "inbox.org")) "* %? %t \n %i\n %a" ) 
+        ("t" "Task" entry (file+headline  (concat GTD_PATH "task.org") "Tasks") "** TODO %?  %t\n %i\n %a" ) 
+        ("p" "Project" entry (file (concat GTD_PATH "project.org"))  "** %? %^g %i\n %a") 
+ ;       ("New" ?n "* %? %t \n %i\n %a" (concat GTD_PATH "inbox.org") ) 
+ ;       ("Task" ?t "** TODO %?\n %i\n %a" (concat GTD_PATH "task.org") "Tasks") 
+ ;       ("Calendar" ?c "** TODO %?\n %i\n %a" (concat GTD_PATH "task.org") "Tasks") 
+ ;       ("Idea" ?i "** %?\n %i\n %a" (concat GTD_PATH "task.org") "Ideas") 
+ ;       ("Note" ?r "* %?\n %i\n %a" (concat GTD_PATH "note.org") ) 
+ ;       ("Project" ?p "** %?\n %i\n %a" (concat GTD_PATH "project.org") %g) 
+     )
+) 
+(setq org-default-notes-file (concat org-directory "inbox.org"))
+;;设置TODO关键字
+(setq org-todo-keywords
+(list "TODO(t)" "|" "CANCELED(c)" "DONE(d)"))
+;; 将项目转接在各文件之间，方便清理和回顾。
+(custom-set-variables
+'(org-refile-targets
+(quote
+(("inbox.org" :level . 1)("task.org" :level . 2) ("project.org":level . 1))
+)))
+;; 快速打开inbox
+(defun inbox() (interactive) (find-file (concat GTD_PATH "inbox.org")))
+(global-set-key "\C-cz" 'inbox)
+;; 快速启动 agenda-view
+(define-key global-map "\C-ca" 'org-agenda-list)
+(define-key global-map "\C-ct" 'org-todo-list)
+(define-key global-map "\C-cm" 'org-tags-view)
+;;显示他们的内容
+(setq org-agenda-files
+    (list (concat GTD_PATH "inbox.org")
+          (concat GTD_PATH "task.org")
+          (concat GTD_PATH "project.org")
+    )
+)
+;;---for org gtd end---
